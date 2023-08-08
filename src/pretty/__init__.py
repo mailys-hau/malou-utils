43ad845214236142eb535bafb79f28e04ce8f14b
mailys-hau <27966914+mailys-hau@users.noreply.github.com>
2023-08-08 15:28:24 +0200

    theme: add some colors

--- src/pretty/__init__.py
+++ src/pretty/__init__.py
@@ -4,10 +4,13 @@ from rich.theme import Theme
 
 
 _theme = Theme({"error": "red3", "warn": "dark_orange", "good": "green3",
-                "sassy": "italic", "happy": "light_coral",
+                "sassy": "italic", "sassy-err": "bold red3",
+                "happy": "light_coral",
+                "dir": "blue bold",
                 "repr.number": ""})
 CONSOLE = Console(theme=_theme)
 
 
 
 from pretty.tables import get_table
+from pretty.progresses import *
